a3a8dc4dbf6c9220eecee546b8658d71f0830921
Nora Skjevik <nosk0001@stud.kea.dk>
2024-02-05 19:40:00 +0100

    init

---
+++ script.js
@@ -0,0 +1,41 @@
+window.addEventListener("DOMContentLoaded", init);
+
+const clothesURL = "https://kea-alt-del.dk/t7/api/products";
+
+let clothesTemplate;
+let clothesContainer;
+
+//hämta kod från json url^^^^ (?)
+function init() {
+  console.log("init");
+
+  clothesTemplate = document.querySelector(".clothes_template");
+  console.log("clothesTemplate", clothesTemplate);
+
+  clothesContainer = document.querySelector(".clothes_container");
+  console.log("clothes_container", clothesContainer);
+
+  fetch(clothesURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      showclothes(json);
+    });
+}
+
+function showclothes(clothesJSON) {
+  let clothesClone;
+
+  clothesJSON.forEach((clothes) => {
+    console.log("clothes", clothes);
+    clothesClone = clothesTemplate.cloneNode(true).content;
+    clothesClone.querySelector(".clothes_image").src = `https://kea-alt-del.dk/t7/images/webp/640/${clothes.id}.webp`;
+    clothesClone.querySelector(".clothes_image").alt = `Picture of a ${clothes.name} clothes`;
+    clothesClone.querySelector(".clothes_name").textContent = clothes.productdisplayname;
+    clothesClone.querySelector(".clothes_price span").textContent = clothes.price;
+    clothesClone.querySelector(".clothes_brandname").textContent = clothes.brandname;
+    clothesClone.querySelector("a").href = `single.html?id=${clothes.id}`;
+    clothesContainer.appendChild(clothesClone);
+  });
+}
