924e91204fcf828e8127835f9c7984beb4d00d52
Nora Skjevik <nosk0001@stud.kea.dk>
2024-02-08 18:51:13 +0100

    kategorisiden js

---
+++ kategori.js
@@ -0,0 +1,18 @@
+fetch(`https://kea-alt-del.dk/t7/api/seasons`)
+  .then((response) => response.json())
+  .then(dataRecieved);
+
+function dataRecieved(data) {
+  data.forEach(showCategories);
+}
+
+function showCategories(showCategory) {
+  const myCategory = document.querySelector("template").content;
+  const myClone = myCategory.cloneNode(true);
+
+  myClone.querySelector("h2").textContent = showCategory.season;
+  myClone.querySelector("a").href = `produktliste.html?Urlinfo=${showCategory.season}`;
+
+  const parentElement = document.querySelector("main");
+  parentElement.appendChild(myClone);
+}
